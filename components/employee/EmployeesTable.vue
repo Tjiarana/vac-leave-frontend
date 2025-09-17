a3e75ccedd6b7de5bd7c803672a3a70042a27b49
Teerasak Jiarananon <Teerasak_Jia@freewillgroup.com>
2025-09-17 16:54:18 +0700

    adjust table height

--- components/employee/EmployeesTable.vue
+++ components/employee/EmployeesTable.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import type { PropType } from 'vue';
+import { useDisplay } from 'vuetify';
+
+const { xs, sm, md, lg, lgAndDown, xl, width, name } = useDisplay()
+
 
 const props = defineProps({
   tableHeaders: {
@@ -12,11 +16,18 @@ const props = defineProps({
   }
 })
 
+const tableHeight = computed(() => {
+  if (xs.value || sm.value) return 300
+  if (md.value) return 400
+  if (lg.value) return 235
+  if (xl.value) return 300
+})
+
 </script>
 
 <template>
-  <v-data-table :headers="props.tableHeaders" :items="props.employees" height="449" item-value="name" fixed-header
-    class="border rounded overflow-hidden employee-list-table">
+  <v-data-table :height="tableHeight" :headers="props.tableHeaders" items-per-page="5" :items="props.employees" item-value="id" fixed-header :density="lgAndDown?'comfortable':'default'" :hover="true"
+    class="border rounded overflow-hidden">
     <template #item.actions="{ item }">
       <div class="d-flex justify-center ga-3">
         <v-tooltip text="แก้ไข">
--- pages/employee.vue
+++ pages/employee.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import EmployeesTable from '~/components/employee/EmployeesTable.vue'
 import { useApi } from '@/composables/useApi'
+import { useDisplay } from 'vuetify'
+
+const { xs, sm, md, lg, lgAndDown, xl, width, name } = useDisplay()
+
 definePageMeta({
   layout: "main",
   middleware: "auth"
@@ -33,10 +37,10 @@ const headers = ref<any[]>([
   <VContainer fluid class="pa-0 bg-surface fill-height d-flex flex-column">
     <v-row no-gutters justify="center" align="center" class="bg-secondary w-100">
       <v-col class="d-flex justify-center">
-        <h1 class="text-h4 font-weight-bold text-white">จัดการพนักงาน</h1>
+        <h1 class="font-weight-bold text-white" :class="lgAndDown?'py-6 text-h5':'py-0'">จัดการพนักงาน</h1>
       </v-col>
     </v-row>
-    <v-row no-gutters class="pa-16 w-100 flex-grow-1">
+    <v-row no-gutters class="w-100 flex-grow-1 px-16" :class="lgAndDown?'py-8':'py-16'">
       <v-col class="d-flex flex-column ga-6">
         <h3 class="font-weight-regular text-secondary ps-4">รายชื่อพนักงาน</h3>
         <v-row no-gutters justify="center" align="center">
@@ -46,7 +50,6 @@ const headers = ref<any[]>([
         </v-row>
       </v-col>
     </v-row>
-    <v-divider />
   </VContainer>
 </template>
 
